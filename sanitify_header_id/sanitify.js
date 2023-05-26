4bafd55b04013eb8746711e59405d3f7c97ec509
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2023-05-26 09:01:34 +0800

    Order matters

--- sanitify_header_id/sanitify.js
+++ sanitify_header_id/sanitify.js
@@ -1,10 +1,12 @@
 function sanitify(s) {
+    // Remove emojis (such a headache)
+    s = s.replaceAll(/([\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2580-\u27BF]|\uD83E[\uDD10-\uDEFF]|\uFE0F| )/g, "");
     // Trim spaces and newlines
     s = s.trim();
     // Replace spaces
     s = s.replaceAll(" ", "-");
     s = s.replaceAll("%20", "-");
-    // Remove emojis
-    s = s.replaceAll(/([\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2580-\u27BF]|\uD83E[\uDD10-\uDEFF]|\uFE0F| )+-/g, '');
+    // No more multiple "-"
+    s = s.replaceAll(/-+/g, "-");
     return s;
 }
