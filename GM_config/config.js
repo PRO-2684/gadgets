b51ba80a290cd21139aa2a2a6d327733ce0fe34a
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2023-11-30 09:35:35 +0800

    Code optimize

--- GM_config/config.js
+++ GM_config/config.js
@@ -3,7 +3,7 @@
 // @name:zh-CN   Tampermonkey 配置
 // @license      gpl-3.0
 // @namespace    http://tampermonkey.net/
-// @version      0.5.3
+// @version      0.5.4
 // @description  Simple Tampermonkey script config library
 // @description:zh-CN  简易的 Tampermonkey 脚本配置库
 // @author       PRO
@@ -59,18 +59,16 @@ function versionCompare(v1, v2, options) {
     return 0;
 }
 function supports(minVer) { // Minimum version of Tampermonkey required
-    return typeof GM_info === "object" && GM_info.scriptHandler === "Tampermonkey" && versionCompare(GM_info.version, minVer) >= 0;
+    return typeof GM_info === "object" // Has API GM_info
+        && GM_info.scriptHandler === "Tampermonkey" // Tampermonkey is detected
+        && versionCompare(GM_info.version, minVer) >= 0; // Compare version
 }
 const supportsOption = supports("4.20.0");
 debug(`Tampermonkey ${GM_info.version} detected, ${supportsOption ? "supports" : "does not support"} menu command options`);
-const registerMenuCommand = supportsOption ? GM_registerMenuCommand : (name, func, option) => GM_registerMenuCommand(name, func, option && option.accessKey);
+const registerMenuCommand = supportsOption ? GM_registerMenuCommand : (name, func, option) => GM_registerMenuCommand(name, func, option.accessKey);
+
 function _GM_config_get(config_desc, prop) {
-    let value = GM_getValue(prop, undefined);
-    if (value !== undefined) {
-        return value;
-    } else {
-        return config_desc[prop].value;
-    }
+    return GM_getValue(prop, config_desc[prop].value);
 }
 const _GM_config_builtin_processors = {
     same: (v) => v,
@@ -109,9 +107,9 @@ const _GM_config_builtin_formatters = {
 const _GM_config_wrapper = {
     get: function (target, prop) {
         // Return stored value, else default value
-        let value = _GM_config_get(target, prop);
+        const value = _GM_config_get(target, prop);
         // Dispatch get event
-        let event = new CustomEvent(GM_config_event, {
+        const event = new CustomEvent(GM_config_event, {
             detail: {
                 type: "get",
                 prop: prop,
@@ -121,11 +119,10 @@ const _GM_config_wrapper = {
         });
         window.top.dispatchEvent(event);
         return value;
-    }
-    , set: function (desc, prop, value) {
+    }, set: function (desc, prop, value) {
         // Dispatch set event
-        let before = _GM_config_get(desc, prop);
-        let event = new CustomEvent(GM_config_event, {
+        const before = _GM_config_get(desc, prop);
+        const event = new CustomEvent(GM_config_event, {
             detail: {
                 type: "set",
                 prop: prop,
@@ -134,7 +131,7 @@ const _GM_config_wrapper = {
             }
         });
         // Store value
-        let default_value = desc[prop].value;
+        const default_value = desc[prop].value;
         if (value === default_value && typeof GM_deleteValue === "function") {
             GM_deleteValue(prop); // Delete stored value if it's the same as default value
             debug(`🗑️ "${prop}" deleted`);
@@ -145,18 +142,16 @@ const _GM_config_wrapper = {
         return true;
     }
 };
-
-let _GM_config_registered = []; // Items: [id, prop]
+const _GM_config_registered = []; // Items: [id, prop]
 // (Re-)register menu items on demand
 function _GM_config_register(desc, config, until = undefined) {
     // `until` is the first property to be re-registered
     // If `until` is undefined, all properties will be re-registered
-    let _GM_config_builtin_inputs = {
+    const _GM_config_builtin_inputs = {
         current: (prop, orig) => { return orig },
         prompt: (prop, orig) => {
-            let s = prompt(`🤔 New value for ${desc[prop].name}:`, orig);
-            if (s === null) return orig;
-            return s;
+            const s = prompt(`🤔 New value for ${desc[prop].name}:`, orig);
+            return s === null ? orig : s;
         },
     };
     // Unregister old menu items
@@ -171,23 +166,23 @@ function _GM_config_register(desc, config, until = undefined) {
             break;
         }
     }
-    for (let prop in desc) {
+    for (const prop in desc) {
         if (prop === until) {
             flag = true;
         }
         if (!flag) continue;
-        let name = desc[prop].name;
-        let orig = _GM_config_get(desc, prop);
-        let input = desc[prop].input || "prompt";
-        let input_func = typeof input === "function" ? input : _GM_config_builtin_inputs[input];
-        let formatter = desc[prop].formatter || "default";
-        let formatter_func = typeof formatter === "function" ? formatter : _GM_config_builtin_formatters[formatter];
-        let option = {
+        const name = desc[prop].name;
+        const orig = _GM_config_get(desc, prop);
+        const input = desc[prop].input || "prompt";
+        const input_func = typeof input === "function" ? input : _GM_config_builtin_inputs[input];
+        const formatter = desc[prop].formatter || "default";
+        const formatter_func = typeof formatter === "function" ? formatter : _GM_config_builtin_formatters[formatter];
+        const option = {
             accessKey: desc[prop].accessKey,
             autoClose: desc[prop].autoClose,
             title: desc[prop].title
         };
-        let id = registerMenuCommand(formatter_func(name, orig), function () {
+        const id = registerMenuCommand(formatter_func(name, orig), function () {
             let value;
             try {
                 value = input_func(prop, orig);
@@ -195,8 +190,8 @@ function _GM_config_register(desc, config, until = undefined) {
                 if (typeof processor === "function") { // Process user input
                     value = processor(value);
                 } else if (typeof processor === "string") {
-                    let parts = processor.split("-");
-                    let processor_func = _GM_config_builtin_processors[parts[0]];
+                    const parts = processor.split("-");
+                    const processor_func = _GM_config_builtin_processors[parts[0]];
                     if (processor_func !== undefined) // Process user input
                         value = processor_func(value, ...parts.slice(1));
                     else // Unknown processor
@@ -219,14 +214,14 @@ function _GM_config_register(desc, config, until = undefined) {
 
 function GM_config(desc, menu = true) { // Register menu items based on given config description
     // Get proxied config
-    let config = new Proxy(desc, _GM_config_wrapper);
+    const config = new Proxy(desc, _GM_config_wrapper);
     // Register menu items
     if (window === window.top) {
         if (menu) {
             _GM_config_register(desc, config);
         } else {
             // Register menu items after user clicks "Show configuration"
-            let id = registerMenuCommand("Show configuration", function () {
+            const id = registerMenuCommand("Show configuration", function () {
                 _GM_config_register(desc, config);
             }, {
                 autoClose: false,
