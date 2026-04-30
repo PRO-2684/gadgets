974c97d540500504bb587a014f4ccb461d0fde6b
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2026-04-30 23:31:48 +0800

    feat(ghp): Using [`relative-time`](https://github.com/github/relative-time-element)

--- github_plus/ghp.js
+++ github_plus/ghp.js
@@ -2,7 +2,7 @@
 // @name         GitHub Plus
 // @name:zh-CN   GitHub 增强
 // @namespace    http://tampermonkey.net/
-// @version      0.5.1
+// @version      0.5.2
 // @description  Enhance GitHub with additional features.
 // @description:zh-CN 为 GitHub 增加额外的功能。
 // @author       PRO-2684
@@ -1065,6 +1065,18 @@
         file_zip:
             '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path d="M3.5 1.75v11.5c0 .09.048.173.126.217a.75.75 0 0 1-.752 1.298A1.748 1.748 0 0 1 2 13.25V1.75C2 .784 2.784 0 3.75 0h5.586c.464 0 .909.185 1.237.513l2.914 2.914c.329.328.513.773.513 1.237v8.586A1.75 1.75 0 0 1 12.25 15h-.5a.75.75 0 0 1 0-1.5h.5a.25.25 0 0 0 .25-.25V4.664a.25.25 0 0 0-.073-.177L9.513 1.573a.25.25 0 0 0-.177-.073H7.25a.75.75 0 0 1 0 1.5h-.5a.75.75 0 0 1 0-1.5h-3a.25.25 0 0 0-.25.25Zm3.75 8.75h.5c.966 0 1.75.784 1.75 1.75v3a.75.75 0 0 1-.75.75h-2.5a.75.75 0 0 1-.75-.75v-3c0-.966.784-1.75 1.75-1.75ZM6 5.25a.75.75 0 0 1 .75-.75h.5a.75.75 0 0 1 0 1.5h-.5A.75.75 0 0 1 6 5.25Zm.75 2.25h.5a.75.75 0 0 1 0 1.5h-.5a.75.75 0 0 1 0-1.5ZM8 6.75A.75.75 0 0 1 8.75 6h.5a.75.75 0 0 1 0 1.5h-.5A.75.75 0 0 1 8 6.75ZM8.75 3h.5a.75.75 0 0 1 0 1.5h-.5a.75.75 0 0 1 0-1.5ZM8 9.75A.75.75 0 0 1 8.75 9h.5a.75.75 0 0 1 0 1.5h-.5A.75.75 0 0 1 8 9.75Zm-1 2.5v2.25h1v-2.25a.25.25 0 0 0-.25-.25h-.5a.25.25 0 0 0-.25.25Z"></path></svg>',
     };
+    /**
+     * Uses `relative-time` element to format the given timestamp.
+     * @param {number} timestamp The timestamp to format.
+     * @return {string} The HTML string of the formatted relative time.
+     */
+    function relativeTime(timestamp) {
+        // Convert timestamp to ISO 8601 formatted timestamp
+        const date = new Date(timestamp);
+        const iso = date.toISOString();
+        const fallback = date.toLocaleString();
+        return `<relative-time prefix="" datetime="${iso}">${fallback}</relative-time>`;
+    }
     function extendedUserInfo() {
         const profile = $(
             ".js-profile-editable-replace .js-profile-editable-area > ul.vcard-details",
@@ -1108,10 +1120,10 @@
             return `${repos}, ${gists}`;
         });
         addInfoRow("calendar", "Joined", (info) =>
-            new Date(info.created_at).toLocaleString(),
+            relativeTime(info.created_at),
         );
         addInfoRow("calendar", "Updated", (info) =>
-            new Date(info.updated_at).toLocaleString(),
+            relativeTime(info.updated_at),
         );
         addInfoRow("id_badge", "Node ID", (info) => info.node_id);
     }
@@ -1150,7 +1162,7 @@
                 warn("Failed to fetch repository info:", error);
                 return null;
             });
-        function addRow(icon_name, name, text_cb, title_cb = null) {
+        function addRow(icon_name, name, html_cb, title_cb = null) {
             const h3 = document.createElement("h3");
             h3.classList.add("sr-only", "ghp-extended-repo-info");
             h3.textContent = name;
@@ -1173,7 +1185,7 @@
             }
             fetchPromise.then((info) => {
                 if (info) {
-                    entry.innerHTML = `${icon} ${text_cb(info)}`;
+                    entry.innerHTML = `${icon} ${html_cb(info)}`;
                     if (title_cb) {
                         entry.title = title_cb(info);
                     }
@@ -1209,20 +1221,17 @@
             addRow(
                 "calendar",
                 "Created",
-                (info) =>
-                    "Created: " + new Date(info.created_at).toLocaleString(),
+                (info) => "Created: " + relativeTime(info.created_at),
             );
             addRow(
                 "calendar",
                 "Updated",
-                (info) =>
-                    "Updated: " + new Date(info.updated_at).toLocaleString(),
+                (info) => "Updated: " + relativeTime(info.updated_at),
             );
             addRow(
                 "calendar",
                 "Pushed",
-                (info) =>
-                    "Pushed: " + new Date(info.pushed_at).toLocaleString(),
+                (info) => "Pushed: " + relativeTime(info.pushed_at),
             );
             addRow("id_badge", "Node ID", (info) => info.node_id);
         }
