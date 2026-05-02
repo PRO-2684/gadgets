31b198e46b25500536662a8b63bb8d602f5bd87c
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2026-05-02 16:37:05 +0800

    feat(devcontainer): Note of pwd

--- devcontainer/source-me.sh
+++ devcontainer/source-me.sh
@@ -1,9 +1,9 @@
-# Attach to VSCode devcontainer (bash)
+# Attach to devcontainer (bash)
 devatt() {
     cid=$(docker ps --filter "label=devcontainer.local_folder=$(pwd)" -q | head -n1)
-    [ -n "$cid" ] && docker exec -e TERM=xterm-256color -it "$cid" bash || echo "No devcontainer found"
+    [ -n "$cid" ] && docker exec -e TERM=xterm-256color -it "$cid" bash || echo "No devcontainer found at $(pwd)"
 }
 # List running devcontainers
 devls() {
-  docker ps --filter "label=devcontainer.local_folder"
+    docker ps --filter "label=devcontainer.local_folder"
 }
