d25eb130946531c16757a838cc9e96e52470f999
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2026-05-02 16:48:32 +0800

    feat(devcontainer): Better exit code and logs

--- devcontainer/source-me.sh
+++ devcontainer/source-me.sh
@@ -1,9 +1,18 @@
 # Attach to devcontainer (bash)
 devatt() {
+    local cid
     cid=$(docker ps --filter "label=devcontainer.local_folder=$(pwd)" -q | head -n1)
-    [ -n "$cid" ] && docker exec -e TERM=xterm-256color -it "$cid" bash || echo "No devcontainer found at $(pwd)"
+
+    if [ -z "$cid" ]; then
+        echo "No devcontainer found at $(pwd)." >&2
+        return 1
+    fi
+
+    echo "Attaching to devcontainer with ID $cid at $(pwd)..." >&2
+    docker exec -e TERM="${TERM:-xterm-256color}" -it "$cid" bash -i
 }
 # List running devcontainers
 devls() {
     docker ps --filter "label=devcontainer.local_folder"
+    # --format "table {{.ID}}\t{{.Image}}\t{{.Labels}}"
 }
