ab104bd231d8865e553ce349c25069ba5d0436a9
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2026-06-29 17:39:28 +0800

    fix(bad-tempered): Max conn per server

--- bad-tempered/aria2c.sh
+++ bad-tempered/aria2c.sh
@@ -1,1 +1,1 @@
-aria2c $1 --connect-timeout=2 --max-tries=0 --lowest-speed-limit=50K --max-concurrent-downloads=5 --split=8
+aria2c $1 --connect-timeout=2 --max-tries=0 --lowest-speed-limit=50K --max-concurrent-downloads=8 --max-connection-per-server=8 --split=8
